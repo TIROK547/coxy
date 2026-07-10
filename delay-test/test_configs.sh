85d31dd0e0a257c50cce94126daa9ef07ec711a9
TIROK547 <ilia.gotbi.2006@gmail.com>
2026-07-10 15:46:28 +0330

    config test added

--- delay-test/test_configs.sh
+++ delay-test/test_configs.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-XRAY_KNIFE=/opt/xray-knife/xray-knife   # download from GitHub releases
+XRAY_KNIFE=/usr/bin/xray-knife
 
-$XRAY_KNIFE http -f ../collectors/channels/raw_configs.txt \
+$XRAY_KNIFE http -f ../output/raw_configs.txt \
   --speedtest \
   --sort \
   --type csv \
-  -o ranked.csv \
-  --threads 20
-
-# ranked.csv columns include: protocol, address, port, delay(ms), download, upload, remark, ...
+  -o ../output/ranked.csv \
+  --thread 20
